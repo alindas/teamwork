bd6f6b9db80b880693b24c5474943db84b23ec7c
Rhee <hhrhee@163.com>
2022-05-07 14:22:04 +0800

    feat: modal 组件添加拓展属性

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,25 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import {Button} from './button';
-import {Icon} from './icon';
+import { Button } from './button';
+import { Icon } from './icon';
 import './modal.css';
 
 interface ModelConfigure {
     title: string;
-    body: React.ReactNode;
+    body?: React.ReactNode;
 
-    icon?: 'info'|'warning'|'error';
+    icon?: 'info' | 'warning' | 'error';
     okCaption?: string;
     cancelCaption?: string;
+    showClose?: boolean
 
     onOk: () => any;
     onCancel?: () => void;
 };
 
 export const Modal = {
-    __icons: {info: 'info-circle', warning: 'warning-circle', error: 'error'} as any,
-    __iconColors: {info: 'fg-success', warning: 'fg-warning', error: 'fg-danger'} as any,
+    __icons: { info: 'info-circle', warning: 'warning-circle', error: 'error' } as any,
+    __iconColors: { info: 'fg-success', warning: 'fg-warning', error: 'fg-danger' } as any,
 
     open: (cfg: ModelConfigure) => {
         const mount = document.createElement('div');
@@ -33,7 +34,7 @@ export const Modal = {
         const onSure = () => {
             let ret = cfg.onOk();
             if (ret !== undefined && ret === false) return;
-            closer();            
+            closer();
         };
 
         const onClose = () => {
@@ -43,23 +44,24 @@ export const Modal = {
 
         ReactDOM.render(
             <div className='modal'>
-                <div className='modal-mask'/>
+                <div className='modal-mask' />
                 <div className='modal-dialog'>
                     <div className='modal-header'>
                         <span>
-                            {cfg.icon&&<Icon className={`mr-2 ${Modal.__iconColors[cfg.icon]}`} type={Modal.__icons[cfg.icon]}/>}
+                            {cfg.icon && <Icon className={`mr-2 ${Modal.__iconColors[cfg.icon]}`} type={Modal.__icons[cfg.icon]} />}
                             {cfg.title}
                         </span>
-                        <Icon type='close' onClick={onClose}/>
+                        { cfg.showClose !== false && <Icon type='close' onClick={onClose} /> }
                     </div>
-
-                    <div className='modal-body'>
-                        {cfg.body}
-                    </div>
-
+                    {
+                        cfg.body &&
+                        <div className='modal-body'>
+                            {cfg.body}
+                        </div>
+                    }
                     <div className='modal-footer'>
-                        <Button theme='primary' size='sm' onClick={onSure}>{cfg.okCaption||'确定'}</Button>
-                        <Button className='ml-2' size='sm' onClick={onClose}>{cfg.cancelCaption||'取消'}</Button>
+                        <Button theme='primary' size='sm' onClick={onSure}>{cfg.okCaption || '确定'}</Button>
+                        <Button className='ml-2' size='sm' onClick={onClose}>{cfg.cancelCaption || '取消'}</Button>
                     </div>
                 </div>
             </div>,
