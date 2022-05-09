35bc04a306c00d6f3a217d366337b59ba0e3fb07
Rhee <hhrhee@163.com>
2022-05-09 09:04:13 +0800

    fix: 修正 datetime 组件日期与星期对应错误的问题

--- src/components/datetime.tsx
+++ src/components/datetime.tsx
@@ -251,13 +251,13 @@ DateTime.DayPicker = (props: DateTimeElemProps & { hasTime?: boolean }) => {
             <table className='datepicker-day'>
                 <thead>
                     <tr>
-                        <th>日</th>
                         <th>一</th>
                         <th>二</th>
                         <th>三</th>
                         <th>四</th>
                         <th>五</th>
                         <th>六</th>
+                        <th>日</th>
                     </tr>
                 </thead>
                 <tbody>
